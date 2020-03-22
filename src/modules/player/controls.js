61c1d3fadddfff25ad7dfae3af842126983207ce
mrprokoala <zunigaruizb@gmail.com>
2020-03-23 02:55:52 +0800

    feat: simple player movement

--- src/modules/player/controls.js
+++ src/modules/player/controls.js
@@ -1,3 +1,4 @@
+import * as Matter from "matter-js";
 import Config from "config";
 import Helpers from "../../utils/helpers";
 
@@ -7,53 +8,38 @@ export default class Controls {
   constructor(player) {
     this.player = player;
 
-    (this.left = keyboard("ArrowLeft")),
-      (this.up = keyboard("ArrowUp")),
-      (this.right = keyboard("ArrowRight")),
-      (this.down = keyboard("ArrowDown"));
+    (this.left = keyboard("ArrowLeft")), (this.right = keyboard("ArrowRight"));
 
     //Left
     this.left.press = () => {
-      this.player.sprite.vx = -5;
-      this.player.sprite.vy = 0;
+      Matter.Body.setVelocity(this.player.body, {
+        x: -10,
+        y: 0
+      });
     };
 
     this.left.release = () => {
-      if (!this.right.isDown && this.player.sprite.vy === 0) {
-        this.player.sprite.vx = 0;
-      }
-    };
-
-    //Up
-    this.up.press = () => {
-      this.player.sprite.vy = -5;
-      this.player.sprite.vx = 0;
-    };
-    this.up.release = () => {
-      if (!this.down.isDown && this.player.sprite.vx === 0) {
-        this.player.sprite.vy = 0;
+      if (!this.right.isDown) {
+        Matter.Body.setVelocity(this.player.body, {
+          x: 0,
+          y: 0
+        });
       }
     };
 
     //Right
     this.right.press = () => {
-      this.player.sprite.vx = 5;
-      this.player.sprite.vy = 0;
+      Matter.Body.setVelocity(this.player.body, {
+        x: 10,
+        y: 0
+      });
     };
     this.right.release = () => {
-      if (!this.left.isDown && this.player.sprite.vy === 0) {
-        this.player.sprite.vx = 0;
-      }
-    };
-
-    //Down
-    this.down.press = () => {
-      this.player.sprite.vy = 5;
-      this.player.sprite.vx = 0;
-    };
-    this.down.release = () => {
-      if (!this.up.isDown && this.player.sprite.vx === 0) {
-        this.player.sprite.vy = 0;
+      if (!this.left.isDown) {
+        Matter.Body.setVelocity(this.player.body, {
+          x: 0,
+          y: 0
+        });
       }
     };
   }
--- src/modules/player/player.js
+++ src/modules/player/player.js
@@ -15,8 +15,6 @@ export default class Player {
     this.textures = textures;
     this.sprite = new Sprite(textures["mario_standing.png"]);
     this.sprite.scale.set(SCALE);
-    this.sprite.vx = 0;
-    this.sprite.vy = 0;
 
     this.body = Bodies.rectangle(0, 0, BLOCK_SIZE, BLOCK_SIZE);
 
@@ -26,8 +24,5 @@ export default class Player {
   updatePos() {
     this.sprite.x = this.body.position.x;
     this.sprite.y = this.body.position.y;
-
-    this.sprite.x += this.sprite.vx;
-    this.sprite.y += this.sprite.vy;
   }
 }
